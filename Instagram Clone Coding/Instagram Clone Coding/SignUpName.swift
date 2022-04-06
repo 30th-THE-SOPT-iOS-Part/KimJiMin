93ef5af7c6db76a55c1fd583beaf9838f746d9ba
Jimin Kim <0305jimin@gmail.com>
2022-04-06 19:52:17 +0900

    initial commit

---
+++ Instagram Clone Coding/Instagram Clone Coding/SignUpName.swift
@@ -0,0 +1,29 @@
+//
+//  SignUpName.swift
+//  Instagram Clone Coding
+//
+//  Created by 김지민 on 2022/04/06.
+//
+
+import UIKit
+
+class SignUpName: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
