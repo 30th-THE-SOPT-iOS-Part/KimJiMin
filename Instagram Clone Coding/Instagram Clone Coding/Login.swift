67a3989c3d819cd1b1e6d6937e68b3c5f62588a1
Jimin Kim <0305jimin@gmail.com>
2022-04-09 01:59:36 +0900

    [Feat] 눈 아이콘 버튼으로 SecurityText 토글(#1)

--- Instagram Clone Coding/Instagram Clone Coding/Login.swift
+++ Instagram Clone Coding/Instagram Clone Coding/Login.swift
@@ -7,11 +7,48 @@
 
 import UIKit
 
+extension UITextField {
+
+    @objc func toggleSecureTextEntry(){
+        self.isSecureTextEntry.toggle()
+        let imageName = self.isSecureTextEntry ? "password hidden eye icon" : "password shown eye icon"
+        let subViews = self.subviews
+            print(subViews)
+        for eachView in subViews {
+            if eachView is UIButton {
+                print("Hello")
+                let currentButton = eachView as! UIButton
+                currentButton.setImage(UIImage(named: imageName), for: .normal)
+            }
+        }
+    }
+
+    func setButton(){
+        let imageName = self.isSecureTextEntry ? "password hidden eye icon" : "password shown eye icon"
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x: 10, y: 5, width: 20, height: 20)
+        button.setImage( UIImage(named: imageName), for: .normal)
+        button.addTarget(self, action: #selector(toggleSecureTextEntry), for: .touchUpInside)
+       let buttonCV: UIView = UIView(frame:
+                      CGRect(x: 20, y: 0, width: 40, height: 30))
+        buttonCV.addSubview(button)
+       rightView = buttonCV
+       rightViewMode = .always
+        
+    }
+    
+}
+
 class Login: UIViewController {
 
     @IBOutlet weak var nameInput: UITextField!
     
-    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!{
+        didSet {
+            passwordInput.tintColor = UIColor.lightGray
+            passwordInput.setButton()
+           }
+    }
     
     @IBOutlet weak var gotoWelcomeButton: UIButton!
     
