773f47228f86a6e315365c7aab4427820c8c2f5d
Jimin Kim <0305jimin@gmail.com>
2022-07-04 19:56:59 +0900

    1단계 : view 인스턴스 생성 및 프로퍼티 선언

---
+++ Codebase Practice/Codebase Practice/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Codebase Practice
+//
+//  Created by 김지민 on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let logoImage = UIImage()
+    private let emailTextField = UITextField()
+    private let passwordTextField = UITextField()
+    private let passwordFinderButton = UIButton()
+    private let signInButton = UIButton()
+    private let signUpLabel = UILabel()
+    private let signUpButton = UIButton()
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
