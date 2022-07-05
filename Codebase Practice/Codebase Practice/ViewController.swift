e59febfef6fefb533386cdf371dbc9c50b5b88bb
Jimin Kim <0305jimin@gmail.com>
2022-07-05 17:55:28 +0900

    3단계: view별 UI 기초 설정

--- Codebase Practice/Codebase Practice/ViewController.swift
+++ Codebase Practice/Codebase Practice/ViewController.swift
@@ -19,42 +19,63 @@ class ViewController: UIViewController {
     /* [1단계] view 인스턴스 생성 및 프로퍼티 선언
      - 개인적으로, 일단 필요한 UIView 리스트업하듯이 선언 먼저 쭉 해두고 2단계 갔다가 3단계 하러 돌아오는 게 편한 것 같다.
      
-     
-       [3단계] view 기초 설정.
+       [3단계] view별 UI 기초 설정.
      */
     private let logoImage = UIImageView().then{
         $0.image = UIImage(named: "Instagram Black Logo")
         $0.contentMode = .scaleAspectFit
     }
     
+    //TODO: - TextField 속성 겹치는 건 따로 파일 만들어 커스텀 클래스 만들어두기.
     private let emailTextField = UITextField().then{
         $0.placeholder = "전화번호, 사용자 이름 또는 이메일"
+        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.backgroundColor = .systemGray6
+        $0.layer.cornerRadius = 4
+        $0.layer.borderColor = UIColor.systemGray5.cgColor
+        $0.layer.borderWidth = 1
+        $0.addLeftPadding(10)
     }
     
     private let passwordTextField = UITextField().then{
         $0.placeholder = "비밀번호"
+        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.backgroundColor = .systemGray6
+        $0.layer.cornerRadius = 4
+        $0.layer.borderColor = UIColor.systemGray5.cgColor
+        $0.layer.borderWidth = 1
+        $0.addLeftPadding(10)
     }
     
     private let passwordFinderButton = UIButton().then{
-        $0.setTitle("비밀번호를 잊으셨나요?", for: .normal)
+        $0.setTitle("비밀번호를 잊으셨나요?", for: .normal)//여기까지만 쓰면 안 보임
+        $0.setTitleColor(.systemBlue, for: .normal)//여기까지 쓰면 보이기 시작
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 10)
     }
     
     private let signInButton = UIButton().then{
-        $0.setTitle("로그인", for: .normal)
+        $0.setTitle("로그인", for: .normal)//여기까지만 쓰면 안 보임
+        $0.backgroundColor = .systemBlue//여기까지 쓰면 보이기 시작
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        $0.layer.cornerRadius = 4
     }
     
     private let signUpLabel = UILabel().then{
         $0.text = "계정이 없으신가요?"
+        $0.font = UIFont.systemFont(ofSize: 14)
+        $0.textColor = .systemGray
     }
     
     private let signUpButton = UIButton().then{
         $0.setTitle("가입하기", for: .normal)
+        $0.setTitleColor(.systemBlue, for: .normal)
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 14)
+//        $0.backgroundColor = .systemGray
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
-        self.view.backgroundColor = .white
     }
 }
 
@@ -67,6 +88,7 @@ extension ViewController {
        }
      */
     private func configureUI() {
+        view.backgroundColor = .white
         view.addSubviews([logoImage, emailTextField, passwordTextField, passwordFinderButton, signInButton, signUpLabel, signUpButton])
         
         logoImage.snp.makeConstraints{
@@ -79,23 +101,26 @@ extension ViewController {
             $0.top.equalTo(logoImage.snp.bottom).offset(34)
             $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
             $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(16)
+            $0.height.equalTo(44)
         }
         
         passwordTextField.snp.makeConstraints{
             $0.top.equalTo(emailTextField.snp.bottom).offset(16)
             $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
             $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(16)
+            $0.height.equalTo(44)
         }
         
         passwordFinderButton.snp.makeConstraints{
-            $0.top.equalTo(passwordTextField.snp.bottom).offset(16)
-            $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
+            $0.top.equalTo(passwordTextField.snp.bottom).offset(19)
+            $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(16)
         }
         
         signInButton.snp.makeConstraints{
             $0.top.equalTo(passwordFinderButton.snp.bottom).offset(34)
             $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(16)
             $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).inset(16)
+            $0.height.equalTo(44)
         }
         
         signUpLabel.snp.makeConstraints{
